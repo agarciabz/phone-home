a83a8032bc9d14c72585e1676314888731c21ed8
agarciabz <agusgb93@gmail.com>
2022-06-08 20:39:31 +0200

    Update layout of card

--- apps/phonehome-app/src/app/app.tsx
+++ apps/phonehome-app/src/app/app.tsx
@@ -1,22 +1,33 @@
-import { ChakraProvider, VStack } from '@chakra-ui/react';
+import { ChakraProvider, Spinner, VStack } from '@chakra-ui/react';
 import { Phone } from '@phonehome/api-interfaces';
 import { PhoneItem } from '@phonehome/ui/phone-item';
 import { useEffect, useState } from 'react';
 
 export const App = () => {
   const [phones, setPhones] = useState<Phone[]>([]);
+  const [isLoading, setLoading] = useState(false);
 
   useEffect(() => {
+    setLoading(true);
     fetch('/api/phones')
       .then((r) => r.json())
-      .then(setPhones);
+      .then((res) => {
+        setPhones(res);
+        setLoading(false);
+      });
   }, []);
 
   return (
     <ChakraProvider>
-      <VStack>
-        {phones.map(p => <PhoneItem key={p.id} phone={p}></PhoneItem>)}
-      </VStack>
+      {isLoading ? (
+        <Spinner size="xl"></Spinner>
+      ) : (
+        <VStack py={5}>
+          {phones.map((p) => (
+            <PhoneItem key={p.id} phone={p}></PhoneItem>
+          ))}
+        </VStack>
+      )}
     </ChakraProvider>
   );
 };
--- libs/ui/phone-item/src/lib/ui-phone-item.tsx
+++ libs/ui/phone-item/src/lib/ui-phone-item.tsx
@@ -1,4 +1,4 @@
-import { Box, Heading, Text } from '@chakra-ui/react';
+import { Box, Heading, Text, Image } from '@chakra-ui/react';
 import { Phone } from '@phonehome/api-interfaces';
 
 export interface UiPhoneItemProps {
@@ -8,9 +8,10 @@ export interface UiPhoneItemProps {
 export function PhoneItem(props: UiPhoneItemProps) {
   const { phone } = props;
   return (
-    <Box p={5} shadow='md' borderWidth='1px'>
-      <Heading fontSize='xl'>{phone.manufacturer}</Heading>
-      <Text mt={4}>{phone.name}</Text>
+    <Box p={5} shadow="md" borderWidth="1px">
+      <Image src={phone.img} boxSize="52"></Image>
+      <Heading fontSize="xl">{phone.name}</Heading>
+      <Text mt={4}>{phone.manufacturer}</Text>
     </Box>
   );
 }
